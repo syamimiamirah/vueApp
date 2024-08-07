85ed954a3ebbfef26a265200b523cabfcc70201b
syamimiamirah <syamimiamirahzamros@gmail.com>
2024-08-08 00:26:17 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vueApp/'  // Replace with your actual repository name
+    ? '/vueApp/'  // Your GitHub repository name
     : '/'
 })
